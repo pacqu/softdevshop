558767ab21a3b6db3a5e3e79a00321532e2cc3f8
Justin Pacquing <pacqqu@gmail.com>
2016-03-16 12:32:38 -0400

    more d3 fun

--- templates/d3ex.js
+++ templates/d3ex.js
@@ -3,6 +3,18 @@ d3.selectAll("p").style("color", "white");
 d3.selectAll("p").style("color", function() {
 	return "hsl(" + Math.random() * 360 + ",100%,50%)";
     });
-d3.selectAll("p").style("color", function(d, i) {
+/*d3.selectAll("p").style("color", function(d, i) {
 	return i % 2 ? "#fff" : "#eee";
-    });
+	}); */
+//d3.select("body").transition().style("background-color", "black");
+d3.selectAll("p")
+    .data([4, 8, 15, 16, 23, 42])
+    .style("font-size", function(d) { return d + "px"; });
+
+d3.selectAll(".bar").style("height", "50px");
+d3.selectAll(".bar").style("font", "48px");
+d3.selectAll(".bar").style("text-align", "right");
+d3.selectAll(".bar").style("background-color", "blue");
+d3.selectAll(".bar")
+    .data([50, 150, 320])
+    .style("width", function(d) {return d + "px";} );
