54f7f2217736efa1515363af84c4d88b9594c787
Justin Pacquing <pacqqu@gmail.com>
2016-03-18 08:43:07 -0400

    ayyy

--- templates/d3ex.js
+++ templates/d3ex.js
@@ -1,3 +1,4 @@
+/*
 d3.selectAll("p").style("color", "white");
 //d3.select("body").style("background-color", "black");
 d3.selectAll("p").style("color", function() {
@@ -5,7 +6,7 @@ d3.selectAll("p").style("color", function() {
     });
 /*d3.selectAll("p").style("color", function(d, i) {
 	return i % 2 ? "#fff" : "#eee";
-	}); */
+	}); /
 //d3.select("body").transition().style("background-color", "black");
 d3.selectAll("p")
     .data([4, 8, 15, 16, 23, 42])
@@ -18,3 +19,12 @@ d3.selectAll(".bar").style("background-color", "blue");
 d3.selectAll(".bar")
     .data([50, 150, 320])
     .style("width", function(d) {return d + "px";} );
+*/
+
+var data = [4, 8, 15,16,23, 42];
+var chart = d3.select(".chart");
+var bar =  chart.selectAll("div");
+var barUpdate = bar.data(data);
+var barEnter =  barUpdate.enter().append("div");
+barEnter.style("width", function(d){
+	return d * 10 + "px";});
