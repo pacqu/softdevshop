c309ad4aa748f5e3d27ffec48af5112c47ea08fd
Justin Pacquing <pacqqu@gmail.com>
2016-04-01 12:32:03 -0400

    wow compression

--- hw07/main.py
+++ hw07/main.py
@@ -25,18 +25,32 @@ def f(x):
 #print f(1)
 #print f(1)(3)
 
+
+
+a=f(1)
+b=h(1)
+
+print a(1)
+print b(1)
+
+#TASK:
 def repeat(word):
     return lambda count: word * count
 
 #print repeat("hi")
 #print repeat("hi")(3)
-
+'''
 def r1(count):
     return repeat("hello")(count)
 
 def r2(count):
     return repeat("goodbye")(count)
+'''
+
+r1 = repeat("hello")
+r2 = repeat("goodbye")
 
 print r1(2) #hellohello
 print r2(2) #goodbyegoodbye
 print repeat('cool')(3) #coolcoolcool
+
